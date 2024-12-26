a5c33feacfa7d3f0da4ab1d181745834dd5f2f74
Fernanda Dias <fernandadias9@hotmail.com>
2024-12-26 13:12:32 -0300

    feat: template telas iniciais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateTelasIniciaisComponent } from './components/template-telas-iniciais/template-telas-iniciais.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateTelasIniciaisComponent
   ],
   imports: [
     BrowserModule,
