3fe7f3c690c1dc0c4fedc6264c64e3ace0b824de
Fernanda Dias <fernandadias9@hotmail.com>
2025-02-21 17:49:15 -0300

    feat: inicia o componente

---
+++ src/app/pages/recuperacao-de-senha/recuperacao-de-senha.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recuperacao-de-senha',
+  templateUrl: './recuperacao-de-senha.component.html',
+  styleUrl: './recuperacao-de-senha.component.scss'
+})
+export class RecuperacaoDeSenhaComponent {
+
+}
