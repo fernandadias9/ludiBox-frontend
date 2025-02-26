6194ba454cedcc0a8f16c65828eb4fb24d2c6e02
Fernanda Dias <fernandadias9@hotmail.com>
2025-02-26 19:28:39 -0300

    refactor: continuando estilização

--- src/app/pages/tela-inicial/tela-inicial.component.ts
+++ src/app/pages/tela-inicial/tela-inicial.component.ts
@@ -11,9 +11,29 @@ export class TelaInicialComponent {
   userImage = '';
   menuOpen = false;
 
-  cards = new Array(18).fill({ title: 'Anúncio', price: 'R$ 100,00' });
+  cards = new Array(100).fill({ title: 'Anúncio', price: 'R$ 100,00' });  // Simulando 100 itens
+  itemsPerPage = 20;
+  currentPage = 1;
+
+  get totalPages(): number {
+    return Math.ceil(this.cards.length / this.itemsPerPage);
+  }
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+
+  getPaginatedCards() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.cards.slice(startIndex, endIndex);
+  }
+
+  changePage(direction: string) {
+    if (direction === 'next' && this.currentPage < this.totalPages) {
+      this.currentPage++;
+    } else if (direction === 'prev' && this.currentPage > 1) {
+      this.currentPage--;
+    }
   }
 }
