fb32ceb7921423d49f08b2c49e21cf83dc956758
Icaleb1 <calebfernandes080@gmail.com>
2025-06-24 20:50:55 -0300

    fix: imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -67,6 +67,8 @@ import { SelectComponent } from './components/select/select.component';
 import { ModalConfirmacaoComponent } from './components/modal-confirmacao/modal-confirmacao.component';
 import { TelaAdmLocacoesComponent } from './pages/tela-adm-locacoes/tela-adm-locacoes.component';
 import { AvaliacaoModalComponent } from './components/avaliacao-modal/avaliacao-modal.component';
+import { LocacaoListagemComponent } from './pages/locacao-listagem/locacao-listagem.component';
+import { ModalDenunciaComponent } from './components/modal-denuncia/modal-denuncia.component';
 
 export const MY_FORMATS = {
   parse: {
@@ -116,7 +118,8 @@ export const MY_FORMATS = {
     SelectComponent,
     ModalDenunciaComponent,
     ModalConfirmacaoComponent,
-    TelaAdmLocacoesComponent
+    TelaAdmLocacoesComponent,
+    AvaliacaoModalComponent
   ],
   imports: [
     BrowserModule,
