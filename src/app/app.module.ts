362ce44b7c38471640fc88fd1784e08f02087532
Fernanda Dias <fernandadias9@hotmail.com>
2025-07-02 22:04:30 -0300

    resolve conflitos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -72,6 +72,8 @@ import { ModalDenunciaComponent } from './components/modal-denuncia/modal-denunc
 import { QrCodeComponent } from './components/qr-code/qr-code.component';
 import { TelaAdmValorBrutoComponent } from './pages/tela-adm-valor-bruto/tela-adm-valor-bruto.component';
 import { TotpModalComponent } from './components/totp-modal/totp-modal.component';
+import { StateAbbrPipe } from './utils/pipeSiglaEstado';
+import { TelaPagamentosAdmComponent } from './pages/tela-pagamentos-adm/tela-pagamentos-adm.component';
 
 export const MY_FORMATS = {
   parse: {
@@ -125,7 +127,9 @@ export const MY_FORMATS = {
     AvaliacaoModalComponent,
     QrCodeComponent,
     TelaAdmValorBrutoComponent,
-    TotpModalComponent
+    TotpModalComponent,
+    StateAbbrPipe,
+    TelaPagamentosAdmComponent
   ],
   imports: [
     BrowserModule,
